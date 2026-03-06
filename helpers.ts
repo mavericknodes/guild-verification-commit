4de5b336585e2325cad1fed9371847d73ece0399
mavericknodes <166492896+mavericknodes@users.noreply.github.com>
2026-03-07 00:16:34 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T21:15:08.576Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T21:16:34.641Z
+Commit: Resolve timezone issues in date handling
