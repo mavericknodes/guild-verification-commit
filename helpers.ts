98885eea16bbd7895cd18818c23f4d63adcba1c2
mavericknodes <166492896+mavericknodes@users.noreply.github.com>
2026-03-07 00:15:08 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T21:15:00.004Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-06T21:15:08.576Z
+Commit: Fix broken links in documentation
