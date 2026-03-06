fd4370f43b4f3649d5d860f3bfeca452bce1d5a4
mavericknodes <166492896+mavericknodes@users.noreply.github.com>
2026-03-07 00:16:42 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T21:15:03.577Z
-Commit: Add multi-language support
+Timestamp: 2026-03-06T21:16:41.821Z
+Commit: Resolve timezone issues in date handling
