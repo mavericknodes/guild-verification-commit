bb6b75f615fc27ce40d41be953d05c7552680fbf
mavericknodes <166492896+mavericknodes@users.noreply.github.com>
2026-03-07 00:09:00 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T21:08:55.725Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T21:08:59.638Z
+Commit: Resolve null pointer exception in user service
